52c76f53c39b51c9f64c95d63befeb914a99fea6
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-12-02 01:35:14 +0600

    Movie Category button don

--- src/components/Home/MovieCategories.jsx
+++ src/components/Home/MovieCategories.jsx
@@ -15,6 +15,18 @@ const TrendingNow = [
   },
 ];
 
+const category = [
+  "Movies",
+  "Series",
+  "Actions",
+  "Anime",
+  "Sci-Fi",
+  "Kids",
+  "Fiction",
+  "Live",
+  "Studios",
+];
+
 const MovieCategories = () => {
   return (
     <div className="my-[63px] relative">
@@ -54,6 +66,23 @@ const MovieCategories = () => {
               </button>
             </div>
           </div>
+        </div>
+
+        {/* Movie Category Button */}
+
+        <div className="flex my-[43px]">
+          {category.map((category, index) => (
+            <button
+              key={index}
+              className={`  text-[16px] px-[31px] py-[13px] rounded-full font-[700] ${
+                category === "Movies"
+                  ? "bg-primary text-black" // Active button styles
+                  : " text-white" // Inactive button styles
+              }`}
+            >
+              {category}
+            </button>
+          ))}
         </div>
       </Container>
     </div>
